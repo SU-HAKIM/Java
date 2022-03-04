18ea9140b4678ca25e7a37e81f89224f56b840b5
sadiul-hakim <bmsuhakim@gmail.com>
2022-03-04 16:16:07 +0600

    comment

--- Third/BioData.java
+++ Third/BioData.java
@@ -3,8 +3,11 @@ package Third;
 public class BioData {
     public static void main(String[] args) {
         //This is single line comment
+        /*
+        This is multi line comment
+         */
         /**
-         * This is multi line comment
+          Documentational Comment 
          */
     }
 }
