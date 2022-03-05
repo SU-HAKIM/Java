b965c22c69051cd965b0e49a07e20ecce6908df8
sadiul-hakim <bmsuhakim@gmail.com>
2022-03-05 07:48:35 +0600

    Unary Operator

--- Six/Operators.java
+++ Six/Operators.java
@@ -2,7 +2,7 @@ package Six;
 
 public class Operators {
     public static void main(String[] args) {
-        //Operators
+        //!Operators
         //?1.Arithmtic
         // System.out.println(10 % 3);
         int res1;
@@ -15,7 +15,7 @@ public class Operators {
         // System.out.println(res2);
 
         //?Assignment Operator
-        int x;
+        int x,result;
         x = 10;//assign 10
         x += 10;//20
         x -= 5;//15
@@ -23,6 +23,20 @@ public class Operators {
         x /= 3;//10
         x %= 3;//1
 
-        System.out.println(x);
+        // System.out.println(x);
+
+        //?Unary Operators
+        result = +x;//1
+        result = -x;//-1
+        result = -x;//-1
+        // System.out.println(result);
+        result = ++x;//First positive the number then plus one = 2
+        result = x++;//2 (3 after calling again)
+        result = --x;//But does not negative the value
+        System.out.println(result);
+        result = x--;
+
+        // System.out.println(result);
+
     }
 }
