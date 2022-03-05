2efaf9bbe7c79255226be2e1e70bdd37e34bc13b
sadiul-hakim <bmsuhakim@gmail.com>
2022-03-05 06:56:04 +0600

    Arithmetic Operator

---
+++ Six/Operators.java
@@ -0,0 +1,17 @@
+package Six;
+
+public class Operators {
+    public static void main(String[] args) {
+        //Operators
+        //1.Arithmtic
+        System.out.println(10 % 3);
+        int res1;
+        double res2;
+
+        res1 = 20 / 10;
+        res2 = (double) 20 / 10;//Type Casting
+        
+        System.out.println(res1);
+        System.out.println(res2);
+    }
+}
