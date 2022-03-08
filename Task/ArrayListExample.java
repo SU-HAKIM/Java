f2bac7b9b908d4532f1bdaa29f56d473e89f370b
sadiul-hakim <bmsuhakim@gmail.com>
2022-03-08 14:56:14 +0600

    important methods

--- Task/ArrayListExample.java
+++ Task/ArrayListExample.java
@@ -10,31 +10,44 @@ import java.util.Iterator;
 public class ArrayListExample {
     public static void main(String[] args) {
         ArrayList<Integer> number = new ArrayList<Integer>();//Do not specify size
-        //adding elements
+        //?adding elements
 
         number.add(10);
         number.add(20);
         number.add(30);
         number.add(40);
         number.add(2, 100);
-        System.out.println(number.size());
-        System.out.println(number);//Array list is printable like this but Array is not
+        // System.out.println(number.size());
+        // System.out.println(number);//Array list is printable like this but Array is not
 
-        for (int x : number) {
-            System.out.println(x);
-        }
+        // for (int x : number) {
+        //     System.out.println(x);
+        // }
 
         Iterator itr = number.iterator();
 
-        while (itr.hasNext()) {
-            System.out.println(itr.next());
-        }
+        // while (itr.hasNext()) {
+        //     System.out.println(itr.next());
+        // }
 
-        //Removing Items
-        number.remove(2);
-        System.out.println(number);
-        number.removeAll(number);
-        System.out.println(number);
+        //?Removing Items
+        // number.remove(2);
+        // System.out.println(number);
+        // number.removeAll(number);
+        // System.out.println(number);
 
+        //?Clear
+        // number.clear();
+        // // System.out.println(number);
+        
+        //* System.out.println(number.isEmpty());
+
+        //* System.out.println(number.contains(20));
+
+        //* System.out.println(number.indexOf(30));
+
+        number.set(3, 50);
+        // System.err.println(number);
+        //* System.err.println(number.get(3));
     }
 }
