44f90bd3c6db69c23e7aea0cc2a753bd04a95bf7
sadiul-hakim <bmsuhakim@gmail.com>
2022-03-09 15:19:49 +0600

    Time

--- date/DateClass.java
+++ date/DateClass.java
@@ -1,7 +1,23 @@
 package date;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class DateClass {
     public static void main(String[] args) {
-        
+        Date date = new Date();
+        // System.out.println(date);
+
+        DateFormat format = new SimpleDateFormat("dd/MM/YYYY");//Formatter
+        String curDate=format.format(date);//Formatted date
+        // System.out.println(curDate);
+
+        LocalTime time = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+        String formattedTime=formatter.format(time);
+        System.out.println(formattedTime);
     }
 }
