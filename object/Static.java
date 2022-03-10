e339571297132d11270bfb5e24831fb0819e6721
sadiul-hakim <bmsuhakim@gmail.com>
2022-03-10 12:12:04 +0600

    Static property

--- object/Static.java
+++ object/Static.java
@@ -10,9 +10,16 @@ public class Static {
         Student s2 = new Student("Hakim2", 3241);
         s2.displayInformation();
         s2.showCount();
-        
+
         Student s3 = new Student("Hakim3", 3261);
         s3.displayInformation();
         s3.showCount();
     }
 }
+
+
+/**
+ * 1. Static variables can be called using class name
+ * 2. Static Variables do not take extra memory space for same property
+ * 3. They are always same do not start from first when new instance is created
+ */
